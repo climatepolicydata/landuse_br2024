a6eb29076f98054699b40fb3bd928fadd7e27456
EduMinsky <84152660+EduMinsky@users.noreply.github.com>
2024-02-05 10:45:46 -0300

    Add files via upload

--- AuxFolder/Creation_Keywords_Activity_LandScape.R
+++ AuxFolder/Creation_Keywords_Activity_LandScape.R
@@ -4,16 +4,21 @@ library(stopwords)
 library(openxlsx)
 library(readxl)
 library(stringi)
+####################################################################################################
+# Script para criar um dicionário de palavras para as bases de dados do BNDES,SIOP, OCDE,NINT e FNMC#
+# Vamos criar um dicionario especifico para cada base contendo as palavras de cada setor landscape  #
+#####################################################################################################
 # Lendo o RDS do ultimo landscape criado
-last_landscape <- read_rds("C:/Users/napcc/Dropbox (CPI)/EduardoMinsky/PARAMIM/brlanduse_landscape2024/AuxFolder/Dict/base_landscape_final_01022024.rds")
-
-last_landscape%>%select(sector_landscape)%>%unique%>%view
-last_landscape%>%filter(sector_landscape == "Livestock")%>%select(data_source)%>%unique
-# instrument_original
-# sector_original
-# subsector_original
-# project_description
-
+last_landscape <- read_rds("./brlanduse_landscape2024_dados/Dict/base_landscape_final_01022024.rds")
+last_landscape <- last_landscape %>% mutate(sector_landscape= case_when(
+  sector_landscape == "crop" ~ "Crop",sector_landscape == "forest" ~ "Forest", sector_landscape=="cattle" ~ "Cattle",
+  sector_landscape == "Bioenergy and fuels" | sector_landscape == "Bioenergy And Fuels" ~ "Bioenergy and Fuels",sector_landscape == "Agriculture" ~ "Crop",.default = sector_landscape
+))
+# Criando stopwords
+pt_stop <- stopwords('pt')%>%
+  as_tibble
+##################################################################################################################################
+########################################################## Funções ###############################################################
 get_ngrama <- function(data_frame,n_value){
   ngrama <- data_frame %>% 
   unnest_tokens(output = value,
@@ -29,114 +34,66 @@ get_ngrama <- function(data_frame,n_value){
                              
   return(ngrama)
 }
-# Criando stopwords
-pt_stop <- stopwords('pt')%>%
-  as_tibble
-# Separando para os setores landscape
-pecuaria <- last_landscape%>%filter(sector_landscape == "Pecuária")%>%select(sector_original,subsector_original,instrument_original)%>%
-                        mutate(sector_original =str_trim(str_to_lower(stri_trans_general(sector_original,"Latin-ASCII"))),
-                         subsector_original = str_trim(str_to_lower(stri_trans_general(subsector_original,"Latin-ASCII"))),
-                         instrument_original =str_trim(str_to_lower(stri_trans_general(instrument_original,"Latin-ASCII"))) )
 
-multisetorial <- last_landscape%>%filter(sector_landscape == "Multisetorial")%>%select(sector_original,subsector_original,instrument_original)%>%
-                        mutate(sector_original =str_trim(str_to_lower(stri_trans_general(sector_original,"Latin-ASCII"))),
-                         subsector_original = str_trim(str_to_lower(stri_trans_general(subsector_original,"Latin-ASCII"))),
-                         instrument_original =str_trim(str_to_lower(stri_trans_general(instrument_original,"Latin-ASCII"))) )
-
-conservacao_rest_reflora <- last_landscape%>%filter(sector_landscape == "Conservação, Restauração e Reflorestamento")%>%select(sector_original,subsector_original,instrument_original)%>%
-                        mutate(sector_original =str_trim(str_to_lower(stri_trans_general(sector_original,"Latin-ASCII"))),
-                         subsector_original = str_trim(str_to_lower(stri_trans_general(subsector_original,"Latin-ASCII"))),
-                         instrument_original =str_trim(str_to_lower(stri_trans_general(instrument_original,"Latin-ASCII"))) )
-
-bio_energ_comb <- last_landscape%>%filter(sector_landscape == "Bioenergia e Combustíveis")%>%select(sector_original,subsector_original,instrument_original)%>%
-                        mutate(sector_original =str_trim(str_to_lower(stri_trans_general(sector_original,"Latin-ASCII"))),
-                         subsector_original = str_trim(str_to_lower(stri_trans_general(subsector_original,"Latin-ASCII"))),
-                         instrument_original =str_trim(str_to_lower(stri_trans_general(instrument_original,"Latin-ASCII"))) )
-
-agricultura <- last_landscape%>%filter(sector_landscape == "Agricultura")%>%select(sector_original,subsector_original,instrument_original)%>%
-                        mutate(sector_original =str_trim(str_to_lower(stri_trans_general(sector_original,"Latin-ASCII"))),
-                         subsector_original = str_trim(str_to_lower(stri_trans_general(subsector_original,"Latin-ASCII"))),
-                         instrument_original =str_trim(str_to_lower(stri_trans_general(instrument_original,"Latin-ASCII"))) )
-
-#Criando o dicionario individual para cada setor landscape
-# Pecuaria
-Palavras_pecuaria<-tibble(Palavras = c(pecuaria$sector_original,pecuaria$subsector_original))
-Palavras_pecuaria <- get_ngrama(Palavras_pecuaria,n_value = 1)
-Palavras_pecuaria_inst_fin <- tibble(Palavras = c(pecuaria$instrument_original))
-Palavras_pecuaria_inst_fin <- get_ngrama(Palavras_pecuaria_inst_fin,n_value = 3)
-Palavras_pecuaria_all <- rbind(Palavras_pecuaria,Palavras_pecuaria_inst_fin)
-Palavras_pecuaria_all$Sector_Landscape = "Pecuaria"
-
-# multisetorial
-Palavras_multisetorial<-tibble(Palavras = c(multisetorial$sector_original,multisetorial$subsector_original))
-Palavras_multisetorial <- get_ngrama(Palavras_multisetorial,n_value = 1)
-Palavras_multisetorial_inst_fin <- tibble(Palavras = c(multisetorial$instrument_original))
-Palavras_multisetorial_inst_fin <- get_ngrama(Palavras_multisetorial_inst_fin,n_value = 3)
-Palavras_multisetorial_all <- rbind(Palavras_multisetorial,Palavras_multisetorial_inst_fin)
-Palavras_multisetorial_all$Sector_Landscape = "Multisetorial"
-
-# conservacao_rest_reflora
-Palavras_conservacao_rest_reflora<-tibble(Palavras = c(conservacao_rest_reflora$sector_original,conservacao_rest_reflora$subsector_original))
-Palavras_conservacao_rest_reflora <- get_ngrama(Palavras_conservacao_rest_reflora,n_value = 1)
-Palavras_conservacao_rest_reflora_inst_fin <- tibble(Palavras = c(conservacao_rest_reflora$instrument_original))
-Palavras_conservacao_rest_reflora_inst_fin <- get_ngrama(Palavras_conservacao_rest_reflora_inst_fin,n_value = 3)
-Palavras_conservacao_rest_reflora_all <- rbind(Palavras_conservacao_rest_reflora,Palavras_conservacao_rest_reflora_inst_fin)
-Palavras_conservacao_rest_reflora_all$Sector_Landscape = "conservacao_rest_reflora"
-
-# bio_energ_comb
-Palavras_bio_energ_comb<-tibble(Palavras = c(bio_energ_comb$sector_original, bio_energ_comb$subsector_original))
-Palavras_bio_energ_comb <- get_ngrama(Palavras_bio_energ_comb,n_value = 1)
-Palavras_bio_energ_comb_inst_fin <- tibble(Palavras = c(bio_energ_comb$instrument_original))
-Palavras_bio_energ_comb_inst_fin <- get_ngrama(Palavras_bio_energ_comb_inst_fin,n_value = 3)
-Palavras_bio_energ_comb_all <- rbind(Palavras_bio_energ_comb,Palavras_bio_energ_comb_inst_fin)
-Palavras_bio_energ_comb_all$Sector_Landscape = "bio_energ_comb"
-
-# agricultura
-Palavras_agricultura<-tibble(Palavras = c(agricultura$sector_original, agricultura$subsector_original))
-Palavras_agricultura <- get_ngrama(Palavras_agricultura,n_value = 1)
-Palavras_agricultura_inst_fin <- tibble(Palavras = c(agricultura$instrument_original))
-Palavras_agricultura_inst_fin <- get_ngrama(Palavras_agricultura_inst_fin,n_value = 3)
-Palavras_agricultura_all <- rbind(Palavras_agricultura,Palavras_agricultura_inst_fin)
-Palavras_agricultura_all$Sector_Landscape = "agricultura"
-
-# Juntando todas as tibbles em um pivot wider
-dicionario_palavras_SectorLandscape<- rbind(Palavras_pecuaria_all,Palavras_multisetorial_all,
-                                            Palavras_conservacao_rest_reflora_all,Palavras_bio_energ_comb_all,Palavras_agricultura_all)%>%pivot_wider(names_from = "Sector_Landscape", values_from ="n" )
-
-dicionario_palavras_SectorLandscape%>%write.xlsx('Dicionarios_palavras_Sector_landscape.xlsx')
-getwd()
-
- # Criando a mesma contagem de palavras mas por TF-IDF
-tf_idf <- rbind(Palavras_pecuaria_all,Palavras_multisetorial_all,
-                                            Palavras_conservacao_rest_reflora_all,Palavras_bio_energ_comb_all,Palavras_agricultura_all)
-
-tf_idf_total_words <- tf_idf%>% 
-  group_by(Sector_Landscape) %>% 
-  summarize(total = sum(n))
-tf_idf <- tf_idf %>% left_join(tf_idf_total_words)
-
-freq_by_rank <- tf_idf %>% 
-  group_by(Sector_Landscape) %>% 
-  mutate(rank = row_number(), 
-         `term frequency` = n/total) %>%
-  ungroup()
-
+get_word_keys <- function(sector, columns_keep, data_frame_){
+  data_frame_sector <- data_frame_ %>% filter(sector_landscape == sector)%>%select(all_of(columns_keep))
+  data_frame_1gram <- data_frame_sector%>%select(columns_keep[1:3])
+  data_frame_1gram_tibble <- tibble(Palavras = unlist(data_frame_1gram,use.names=FALSE))
+  data_frame_3gram <- data_frame_sector%>%select(columns_keep[4])
+  data_frame_3gram_tibble <- tibble(Palavras = unlist(data_frame_3gram,use.names=FALSE))
+  data_frame_dict_1word <- get_ngrama(data_frame =data_frame_1gram_tibble,n_value = 1 )
+  data_frame_dict_3word <- get_ngrama(data_frame =data_frame_3gram_tibble,n_value = 3 )
+  data_frame_dict_1word$Sector = sector
+  data_frame_dict_3word$Sector = sector
+  data_frame_allWords <- rbind(data_frame_dict_1word,data_frame_dict_3word)
+  return(data_frame_allWords)
   
-sector_tf_idf <- tf_idf %>%
-  bind_tf_idf(value, Sector_Landscape, n)
+}
+##################################################################################################################################
+##################################################################################################################################
 
-library(forcats)
+# Para o BNDES n aut:
+bndes_nauT%>%filter(sector_landscape=="Crop")%>% select(project_description,sector_original)%>%unique%>%view
+bndes_nauT <- last_landscape%>%filter(data_source == "bndes_naut")%>%as_tibble()
+bndes_nauT%>%select(sector_landscape)%>%unique
+bndes_wordKeys_bioenergia <- get_word_keys(sector = "Bioenergy and Fuels", columns_keep = c("sector_original","subsector_original","project_description","source_original"),data_frame_ = bndes_nauT)
+bndes_wordKeys_crop <- get_word_keys(sector = "Crop", columns_keep = c("sector_original","subsector_original","project_description","source_original"),data_frame_ = bndes_nauT)
+bndes_wordKeys_forest <- get_word_keys(sector = "Forest", columns_keep = c("sector_original","subsector_original","project_description","source_original"),data_frame_ = bndes_nauT)
+bndes_wordKeys_cattle <- get_word_keys(sector = "Cattle", columns_keep = c("sector_original","subsector_original","project_description","source_original"),data_frame_ = bndes_nauT)
+# Unindo todo mundo e aplicando um pivot_wider:
+bndes_all_words <- rbind(bndes_wordKeys_bioenergia,bndes_wordKeys_crop,bndes_wordKeys_forest,bndes_wordKeys_cattle)
+bndes_all_words <- bndes_all_words%>%mutate(value = str_trim(str_to_lower(stri_trans_general(value,"Latin-ASCII"))))
+bndes_all_words_landuse <- bndes_all_words%>%pivot_wider(names_from="Sector",values_from="n")
+#bndes_all_words_landuse%>%write.xlsx('Dicionario_BNDES_Naut.xlsx')
 
-sector_tf_idf %>%
-  group_by(Sector_Landscape) %>%
-  slice_max(tf_idf, n = 15) %>%
-  ungroup() %>%
-  ggplot(aes(tf_idf, fct_reorder(value, tf_idf), fill = Sector_Landscape)) +
-  geom_col(show.legend = FALSE) +
-  facet_wrap(~Sector_Landscape, ncol = 2, scales = "free") +
-  labs(x = "tf-idf", y = NULL)
-  
-sector_tf_idf%>%select(value,Sector_Landscape,tf_idf)%>%pivot_wider(names_from="Sector_Landscape", values_from="tf_idf")%>%write.xlsx("Dicionarios_palavras_TFIDF_Sector_landscape.xlsx")
+# Aplicando o dicionario
+last_landscape <- last_landscape%>%mutate(Coluna_search = str_c(sector_original,subsector_original,project_description,sep=";"))
+last_landscape%>%filter(data_source == "bndes_naut")%>%filter(sector_landscape=="Forest")
+last_landscape%>%filter((grepl(pattern = "\\bcriacao\\b",x =Coluna_search, ignore.case = TRUE ) & grepl(pattern = "\\bgalinaceos\\b",x =Coluna_search, ignore.case = TRUE)) |
+                              (grepl("\\bcriacao\\b",x =Coluna_search, ignore.case = TRUE) & grepl("\\bovinos\\b",x =Coluna_search, ignore.case = TRUE))|
+                              (grepl("\\bproducao\\b",x =Coluna_search, ignore.case = TRUE) & grepl("\\bovos\\b",x =Coluna_search, ignore.case = TRUE)))%>%view
 
 
 
+last_landscape%>%filter(data_source == "bndes_naut")%>%filter(sector_landscape=="Forest")%>%view
+last_landscape%>%filter(grepl(pattern = "\\bjardim botanico\\b",x =Coluna_search, ignore.case = TRUE ) & grepl(pattern = "\\bzoo\\b",x =Coluna_search, ignore.case = TRUE)&
+                              grepl(pattern = "\\bpq nac\\b",x =Coluna_search, ignore.case = TRUE)& grepl(pattern = "\\breserva eco\\b",x =Coluna_search, ignore.case = TRUE)&
+                              grepl(pattern = "\\bprot ambiental\\b",x =Coluna_search, ignore.case = TRUE))%>%view
+
+
+
+
+all_bndes <- read_rds('./brlanduse_landscape2024_dados/BNDES_N_Aut/operacoes_financiamento_operacoes_nao_automaticas_clear.RDS')
+all_bndes<-all_bndes %>% mutate(ano = as.numeric(format(dmy(all_bndes$data_da_contratacao),'%Y')))
+all_bndes <- all_bndes%>%filter(ano >=2015 & ano <=2020)
+all_bndes <- all_bndes%>%mutate(Coluna_search = str_c(subsetor_cnae_nome,subsetor_cnae_agrupado,descricao_do_projeto,sep=";"))
+
+
+all_bndes%>%filter((grepl(pattern = "\\bcriacao\\b",x =Coluna_search, ignore.case = TRUE ) & grepl(pattern = "\\bgalinaceos\\b",x =Coluna_search, ignore.case = TRUE)) |
+                              (grepl("\\bcriacao\\b",x =Coluna_search, ignore.case = TRUE) & grepl("\\bovinos\\b",x =Coluna_search, ignore.case = TRUE))|
+                              (grepl("\\bproducao\\b",x =Coluna_search, ignore.case = TRUE) & grepl("\\bovos\\b",x =Coluna_search, ignore.case = TRUE)))%>%view
+
+
+
+# Rascunho
+bndes_nauT%>%filter(sector_landscape=="Bioenergy and Fuels")%>%select(sector_original)%>%unique()%>%view
