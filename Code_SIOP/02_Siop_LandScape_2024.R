0047a095afca7ec9a987c7d3703e8acffdd5f15e
EduMinsky <84152660+EduMinsky@users.noreply.github.com>
2024-02-06 17:23:57 -0300

    Add files via upload

--- Code_SIOP/02_Siop_LandScape_2024.R
+++ Code_SIOP/02_Siop_LandScape_2024.R
@@ -1,17 +1,17 @@
 library(tidyverse)
 library(stringi)
 library(readxl)
-getwd()
-siop_tratado <- read_rds('./SIOP/Siop_Tratado_2021_2023.rds')
+
+siop_tratado <- read_rds('./brlanduse_landscape2024_dados/SIOP/Siop_Tratado_2021_2023.rds')
 
 #Tabelas relacionais
-source_landscape <- read_excel("./SIOP/12_siop_relational_tables.xlsx", sheet="source_landscape")
+source_landscape <- read_excel("./brlanduse_landscape2024_dados/SIOP/12_siop_relational_tables.xlsx", sheet="source_landscape")
 source_landscape <- source_landscape%>%rename(source_original =`source_of finance_original` )
 source_landscape <- source_landscape%>%mutate(source_original = str_trim(str_to_lower(stri_trans_general(source_original,"Latin-ASCII"))))
 
-channel_landscape <- read_excel("./SIOP/12_siop_relational_tables.xlsx", sheet="channel_landscape")
+channel_landscape <- read_excel("./brlanduse_landscape2024_dados/SIOP/12_siop_relational_tables.xlsx", sheet="channel_landscape")
 
-instrument_landscape <- read_excel("./SIOP/12_siop_relational_tables.xlsx", sheet="instrument_landscape")
+instrument_landscape <- read_excel("./brlanduse_landscape2024_dados/SIOP/12_siop_relational_tables.xlsx", sheet="instrument_landscape")
 
 ######################################################################
 siop_tratado %>% mutate(
@@ -25,41 +25,7 @@ siop_tratado %>% mutate(
            channel_original = str_c(modalidade,und_orc,sep=";")
            )%>%left_join(channel_landscape%>%select(-channel_original),by="modalidade") %>%
            mutate(instrument_original = grupo_de_despesa)%>%left_join(instrument_landscape,by="instrument_original")%>%
-           mutate(sector_original = str_c(funcao,subfuncao,sep = ";"),
-           sector_landscape = case_when(
-            sector_original == "organizacao agraria;assistencia comunitaria "|
-            sector_original == "encargos especiais;outros encargos especiais"|
-            sector_original == "administracao;planejamento e orcamento"|
-            sector_original == "agricultura;extensao rural"|
-            sector_original == "agricultura;meteorologia"|
-            sector_original == "agricultura;abastecimento"|
-            sector_original == "agricultura;defesa agropecuaria"|
-            sector_original == "agricultura;desenvolvimento tecnologico e engenharia"|
-            sector_original == "agricultura;difusao do conhecimento cientifico e tecnologico"|
-            sector_original == "agricultura;normatizacao e fiscalizacao"|
-            sector_original == "agricultura;preservacao e conservacao ambiental"|
-            sector_original == "agricultura;promocao da producao agropecuaria"|
-            sector_original ==  "gestao ambiental;preservacao e conservacao ambiental"|
-            sector_original ==  "orcamento publico;agricultura"|
-            sector_original ==  "organizacao agraria;extensao rural"|
-            sector_original ==  "organizacao agraria;ordenamento territorial" ~ "Agriculture",
-
-            acao == "desenvolvimento da agroenergia"|
-            sector_original == "ciencia e tecnologia;desenvolvimento cientifico"|
-            sector_original ==  "ciencia e tecnologia;desenvolvimento tecnologico e engenharia"|
-            sector_original ==  "direitos da cidadania;assistencia aos povos indigenas"|
-            sector_original ==  "energia;biocombustiveis"|
-            acao ==  "apoio a criacao, gestao e implementacao das unidades de conservacao federais"|
-            sector_original ==  "gestao ambiental;administracai geral" ~ "Bioenergy and Fuels",
-
-            sector_original == "agricultura;irrigacao" |
-            acao == "regularizacao fundiaria e assistencia tecnica e extensao rural na amazonia legal e sua regiao fronteirica"|
-            
-           ))
+           mutate(sector_original = str_c(funcao,subfuncao,sep = ";"))
 
 
-
-siop_tratado%>%mutate(sector_original = str_c(funcao,subfuncao,sep = ";"))%>%select(sector_original)%>%filter(sector_original=='agricultura;extensao rural')
-siop_tratado%>%nrow
-siop_tratado%>%view
 
