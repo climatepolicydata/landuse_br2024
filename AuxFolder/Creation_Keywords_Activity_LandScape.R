f843038f72a890e6e900f00378e6ac37c3354ded
EduMinsky <84152660+EduMinsky@users.noreply.github.com>
2024-02-08 17:31:15 -0300

    Update Creation_Keywords_Activity_LandScape.R

--- AuxFolder/Creation_Keywords_Activity_LandScape.R
+++ AuxFolder/Creation_Keywords_Activity_LandScape.R
@@ -84,3 +84,7 @@ siop_all_words_landuse%>%write.xlsx('C:/Users/napcc/Dropbox (CPI)/EduardoMinsky/
 siop%>%filter(sector_landscape=="Bioenergy and Fuels")%>%select("project_name","project_description","sector_original","subsector_original","channel_original","source_original","sector_landscape")%>%unique%>%view
 siop%>%filter(sector_landscape=="Crop")%>%select("project_name","project_description","sector_original","subsector_original","channel_original","source_original","sector_landscape")%>%unique%>%view
 siop%>%filter(sector_landscape=="Forest")%>%select("project_name","project_description","sector_original","subsector_original","channel_original","source_original","sector_landscape")%>%unique%>%view
+siop%>%filter(sector_landscape=="Multi-sector")%>%select("project_name","project_description","sector_original","subsector_original","channel_original","source_original","sector_landscape")%>%unique%>%view
+
+siop%>%filter(instrument_original == "amortizacao da divida")%>%view
+siop%>%select(instrument_original)%>%unique
