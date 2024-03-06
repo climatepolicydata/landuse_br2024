5a0a1d98de684e23885a14dd013383349c42f683
EduMinsky <84152660+EduMinsky@users.noreply.github.com>
2024-03-06 17:11:34 -0300

    Update 01_get_and_clear_NINT.R

--- Code_NINT/01_get_and_clear_NINT.R
+++ Code_NINT/01_get_and_clear_NINT.R
@@ -13,6 +13,14 @@ nint <- nint %>% mutate(
     categoria = str_to_lower(stri_trans_general(Tema,"Latin-ASCII")),
     tipo_de_emissor = str_to_lower(stri_trans_general(`Tipo de Emissor/Tomador`,"Latin-ASCII")),
     uso_de_recursos = str_to_lower(stri_trans_general(`Uso de Recursos`,"Latin-ASCII")),
-    data = year(`Data de emissão`)
-) %>%view
+    data = year(`Data de emissão`),
+    moeda = str_to_lower(`Moeda ($)`) ,
+    valor = `Valor (M)`,
+    prazo_anos = `Prazo (anos)`,
+    verificador_externo = `Parecer Independente (SPO)`,
+    verificador_externo2 = `Outros Documentos (SPO Framework)`,
+    cbi = `Link CBI`,verificador_cbi = `Verificador CBI`
+) %>% select(number,emissor_trade_name,mercado,instrumento_financeiro,tipo,categoria,tipo_de_emissor,uso_de_recursos,data,moeda,valor,prazo_anos,verificador_externo,verificador_externo2,cbi,verificador_cbi)
 
+
+nint %>% write_csv2("nint_clear.csv")
