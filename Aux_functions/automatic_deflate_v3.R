e75c68bee0d9c52cd95ab0414655642a10d43a3a
Julia de Niemeyer Caldas <52584743+JuliaNiemeyer@users.noreply.github.com>
2025-07-29 14:59:54 -0300

    mudei uma coisinha

--- Aux_functions/automatic_deflate_v3.R
+++ Aux_functions/automatic_deflate_v3.R
@@ -1,6 +1,6 @@
 # Author : Julia Niemeyer & João Valente
 # Date: 20/05/2025
-# Goal: automatic function for index of deflate
+# Goal: automatic function for index of deflate (3 functions for different data)
 # resource:  IPCA do IPEA http://ipeadata.gov.br/Default.aspx
 
 
@@ -99,29 +99,7 @@ deflator_automatico <- function(ano_ini, ano_fim, tabela) {
 }
 
 
-#aplica a funcao de deflator sobre os valores e aplica a tabela de cambio
-# deflate_and_exchange <- function(tabela_deflator, base_select_deflator, tabela_cambio) {
-# 
-#     base_select_deflator <- base_select_deflator %>%
-#       left_join(tabela_deflator, by= "year") %>%
-#       left_join(tabela_cambio, by= "year") %>%
-#       mutate(value_brl_deflated = as.numeric(value_original_currency * deflator),
-#              value_usd = value_brl_deflated/bid)
-# 
-# 
-#     return(base_select_deflator)
-#     cat("deflator e cambio aplicados sobre a base de dados")
-#   }
-
-#DeflateV3
-# caso original_currency == 'BRL', value_BRLm = value_original_currency,  value_brl_deflated = as.numeric(value_original_currency * deflator),  value_USDm= value_BRLm/bid,  
-## Caso original_currency == 'USD', value_USDm = original_currency, 
-
-#deflateUSD
-# caso original_currency == 'BRL',value_USDm_deflated = value_USDm*deflator_USD
-# Caso original_currency == 'USD', value_USDm_deflated = value_USDm*deflator_USD, value_BRLm = value_USDm*bid,  value_brl_deflated = value_USDm_deflated*bid
-
-
+## essa função é para as bases do landuse que tem como valor o value_original_currency
 deflate_and_exchange_Landuse <- function(tabela_deflator, base_select_deflator, tabela_cambio) {
   
   base_select_deflator <- base_select_deflator %>%
@@ -146,6 +124,8 @@ deflate_and_exchange_Landuse <- function(tabela_deflator, base_select_deflator, 
 
 
 
+
+## Mesma função mas para bases que já tem valores de value_USDm e de value_BRLm
 deflate_and_exchange <- function(tabela_deflator, base_select_deflator, tabela_cambio) {
   
   base_select_deflator <- base_select_deflator %>%
@@ -154,7 +134,7 @@ deflate_and_exchange <- function(tabela_deflator, base_select_deflator, tabela_c
     mutate(
       # BRL case
       value_BRLm = ifelse(original_currency == "USD", ## se for em dolar originalmente, o valor de BRL tem que aplicar cambio no original currency, se náo for USD é brl e ai so pega o valor de original em brl
-                          as.numeric(value_BRLm * cambio),
+                          as.numeric(value_USDm * cambio),
                           value_BRLm),
       value_brl_deflated = as.numeric(as.numeric(value_BRLm) * deflator_BRL), #aplica deflacao brasileira sobre BRL
       value_USDm = ifelse(original_currency == "BRL", ## se for em brl o original currency, o valor de USDm tem que aplicar o cambio no original currency, se for dolar, é o valor em dolar.  
@@ -169,23 +149,28 @@ deflate_and_exchange <- function(tabela_deflator, base_select_deflator, tabela_c
 }
 
 
-deflate_and_exchange_AllData <- function(base_select_deflator) {
+
+
+## essa função criei só para rodar na tabela fundida, para tratar dos dados da GLCF. 
+# a diferença é que ela já tem todas as colunas, então cria uma coluna nova com mesmo nome deflator_BRL.y - aí usa essa como base para deflator
+# depois no código mesmo eu junto com deflator_BRL em uma única coluna.
+deflate_and_exchange_AllData <- function(tabela_deflator, base_select_deflator, tabela_cambio) {
   
-  base_select_deflator <- base_select_deflator %>%
-   # left_join(tabela_deflator, by = "year") %>%
-    #left_join(tabela_cambio, by = "year") %>%
+  base_select_deflator <- df_landscape_final %>%
+    left_join(tabela_deflator, by = "year") %>%
+    left_join(tabela_cambio, by = "year") %>%
     mutate(
       # BRL case
-      value_BRLm = ifelse(original_currency == "USD",
-                          as.numeric(value_BRLm * taxa_cambio_dolar),
-                          value_BRLm),
-      value_BRLm_deflated = as.numeric(as.numeric(value_BRLm) * deflator_BRL),
+      value_BRLm = ifelse(original_currency == "usd",
+                          as.numeric(value_USDm * cambio),
+                          value_BRLm) ,
+      value_BRLm_deflated = as.numeric(as.numeric(value_BRLm) * deflator_BRL.y),
       value_USDm = ifelse(original_currency == "BRL",
-                          as.numeric(as.numeric(value_BRLm)/taxa_cambio_dolar),
+                          as.numeric(as.numeric(value_BRLm)/cambio),
                           value_USDm)
-      # USD to BRL (for completeness)
-      
     )
+  
+  
   
   cat("Deflator e câmbio aplicados sobre a base de dados\n")
   return(base_select_deflator)
@@ -194,99 +179,3 @@ deflate_and_exchange_AllData <- function(base_select_deflator) {
 
 
 
-
-
-
-
-
-
-# ########################## directories ############################
-# #criando a funcao para atualizar a tabvela de deflator
-# deflator_automatico <- function(ano_ini, ano_fim, base, current_year) {
-#   # Create character vector for the period
-#   # Build vector
-#   if (ano_ini == ano_fim & current_year == T) {
-#     #print("criando deflator para o ano corrente...")
-#     # Criar a tabela
-#     tabela_final <- data.frame(
-#       year = ano_ini,
-#       deflator = 1
-#     )
-#     
-#     cat("deflator para o ano corrente é igual a 1")
-#     return(tabela_final)
-#     
-#   }
-#   else {
-#     #print("ano_ini diferente de ano_fim ")
-#     # Criar vetor de strings no formato "ano12"
-#     datas <- paste0(ano_ini:ano_fim, "12")
-#     
-#     
-#     ##pegando a tabela atualizada do sidra
-#     sidra <- get_sidra(
-#       x = 1737,                          # Tabela com dados de IPCA por grupos
-#       #variable = 63,                     # Variação acumulada no ano (%)
-#       period = datas,          # últimos 120 meses (ou ajuste como quiser)
-#       geo = "Brazil",                    # Nível Brasil
-#       #classific = "c315",                # Classificação: grupos de produtos
-#       category = list("7169"),           # 7169 = Índice geral
-#       header = T,
-#       format = 3                         # Data.frame limpo
-#     )
-#     
-#     ibge_ipca <- sidra %>%
-#       filter(Variável == "IPCA - Variação acumulada em 12 meses")
-#     
-#     
-#     serie_basedosdados <- ibge_ipca
-#     
-#     # selecao e filtros de valores de interesse ao calculo, queremos sempre a variacao anual, por isso o mes == 12
-#     tabela_final <- serie_basedosdados %>% 
-#       separate(Mês, into = c("mes", "ano"), sep = " ") %>%
-#       select(ano, Valor) %>% 
-#       dplyr::rename("deflator" = "Valor",
-#                     "year" = "ano") %>%
-#       mutate(year = as.integer(year)) %>%
-#       filter(year >= ano_ini & year <= ano_fim ) %>% 
-#       arrange(desc(year))
-#     
-#     if(current_year == T){
-#       # Criar a tabela
-#       tabela_final <- tabela_final %>%
-#         mutate(deflator = if_else(year == ano_fim, 1, deflator))
-#       cat("deflator para o ano corrente é igual a 1\n")
-#     }
-#     
-#     cat("deflator automatico atualizado para", ano_fim, "\n")
-#     return(tabela_final)
-#     
-#   }
-#   }
-# 
-# #aplica a funcao de deflator sobre os valores e aplica a tabela de cambio
-# deflate_and_exchange <- function(tabela_deflator, base_select_deflator, tabela_cambio) {
-#     
-#     base_select_deflator <- base_select_deflator %>% 
-#       left_join(tabela_deflator, by= "year") %>%
-#       left_join(tabela_cambio, by= "year") %>%  
-#       mutate(value_brl_deflated = as.numeric(value_original_currency * deflator),
-#              value_usd = value_brl_deflated/bid)
-#     
-#     
-#     return(base_select_deflator)
-#     cat("deflator e cambio aplicados sobre a base de dados")
-#   }
-#   
-# 
-# #teste <- deflator_automatico(ano_ini, ano_fim, ibge_ipca)
-# # 
-# # print("informe como variavel ano_ini, ano_fim (será a base do deflator) e anos =  seq(ano_fim,ano_ini, -1) para a prox etapa")
-# # 
-# # print("rode: calculo_deflator(tabela_deflator = deflator_automatico(ano_ini, ano_fim, anos), base para aplicacao da funcao))")
-# # 
-# 
-# # 
-# # # write.xlsx(base_deflator_aplicado,"base_deflacionada.xlsx")
-# 
-# 
